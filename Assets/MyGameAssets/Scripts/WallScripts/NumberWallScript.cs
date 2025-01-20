104008caaec616d352a87bd8f9308ec5066c2416
408 <>
2025-01-20 16:22:04 +0900

    Change
    足場をシンプルなキューブに変更
    足場と壁の速度を同じに変更

--- Assets/MyGameAssets/Scripts/WallScripts/NumberWallScript.cs
+++ Assets/MyGameAssets/Scripts/WallScripts/NumberWallScript.cs
@@ -8,24 +8,20 @@ public class NumberWallScript : MonoBehaviour
     private Question question;
 
     [SerializeField]
-    private Rigidbody wallRigidbody;
-    [SerializeField]
     private Collider wallCollider;
-    [SerializeField]
-    private GameObject brokenWall;
 
     [SerializeField]
     private float movePower;
     [SerializeField]
     private float moveSpeed;
-    [SerializeField]
-    private float destroyTime;
 
     private void Update()
     {
         //ï«ÇÃà⁄ìÆ
         //wallRigidbody.AddForce(Vector3.back * ((moveSpeed + wallRigidbody.velocity.z) * movePower), ForceMode.Acceleration);
-        wallRigidbody.velocity = new Vector3(0, 0, -moveSpeed);
+
+        this.gameObject.transform.position -= new Vector3(0, 0, 10 * Time.deltaTime);
+        
 
         //ê≥âÇÃéûÇÕï«Çî≤ÇØÇÁÇÍÇÈ
         if (question != null)
@@ -45,13 +41,9 @@ public class NumberWallScript : MonoBehaviour
     {
         if (collider.gameObject.tag == "Player")
         {
-            var unnecessaryWall = Instantiate(brokenWall, new Vector3(wallCollider.transform.position.x, 0, wallCollider.transform.position.z), this.transform.rotation);
-
             Destroy(this.gameObject);
 
             GameManager.gameManager.PlusCorrectCount();
-
-            Destroy(unnecessaryWall, destroyTime);
         }
     }
 }

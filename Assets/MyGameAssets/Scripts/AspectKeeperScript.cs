30a90240880a07435c05f357057eecbf1058d6b2
Sansan338 <Negame.0816@outlook.jp>
2025-02-06 03:48:33 +0900

    Add
    アスペクト比が固定されるように

--- Assets/MyGameAssets/Scripts/AspectKeeperScript.cs
+++ Assets/MyGameAssets/Scripts/AspectKeeperScript.cs
@@ -11,13 +11,23 @@ public class AspectKeeperScript : MonoBehaviour
 
     void Update()
     {
-        var screenAspect = (float)Screen.width / Screen.height;
+        var screenAspect = Screen.width / (float)Screen.height;
         var targetAspect = aspectVector.x / aspectVector.y;
 
         var magnification = targetAspect / screenAspect;
 
         var viewportRect = new Rect(0, 0, 1, 1);
-        viewportRect.width = magnification;
+
+        if (magnification < 1)
+        {
+            viewportRect.width = magnification;
+            viewportRect.x = 0.5f - viewportRect.width * 0.5f;
+        }
+        else
+        {
+            viewportRect.height = 1 / magnification;
+            viewportRect.y = 0.5f - viewportRect.height * 0.5f;
+        }
         mainCamera.rect = viewportRect;
     }
 }
